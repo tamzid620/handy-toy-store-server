1d474f49b8fd67dd6a2b9e1788ca5baebecd7d5d
tamzid620 <tamzid620@gmail.com>
2023-05-23 10:37:05 +0600

    vercel data problelm fixed

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-// middleware 
-app.use(cors());
+// middleware -------------------------------
+
+// app.use(cors());
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+  app.options("", cors(corsConfig))
 app.use(express.json());
 
 
@@ -14,7 +22,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qtemx5j.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,14 +32,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    client.connect();
+    client.connect((error) => {
+      if (error) {
+        console.log(error)
+        return;
+      }
+    });
 
 
     const toyCollection = client.db('handyToy').collection('toys');
 
     const indexKeys = { seller: 1 };
     const indexOptions = { name: 'name' };
-    const result =  toyCollection.createIndex(indexKeys, indexOptions);
+    const result = toyCollection.createIndex(indexKeys, indexOptions);
+
+
+
+    // all toys ---------------------------------------------------------------
+    app.get('/allToys', async (req, res) => {
+      const cursor = toyCollection.find().limit(20);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    // all toy details ---------------------------------------------------------------
+    app.get("/alltoydetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await toyCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
 
     // toy search ---------------------------------------------------------------
     app.get('/toysearch/:text', async (req, res) => {
@@ -46,17 +76,55 @@ async function run() {
       res.send(result)
     })
 
-        // all toys ---------------------------------------------------------------
-    app.get('/allToys', async (req, res) => {
-      const cursor = toyCollection.find().limit(20);
-      const result = await cursor.toArray();
-      res.send(result);
-    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        // category details ---------------------------------------------------------------
+
+    // add toy ---------------------------------------------------------------
+    app.post('/addToys', async (req, res) => {
+      const body = req.body;
+      const result = await toyCollection.insertOne(body);
+      res.send(result)
+    });
+
+
+    // my toys ---------------------------------------------------------------
+    app.get("/myToys/:email", async (req, res) => {
+      console.log(req, toyCollection);
+      const result = await toyCollection
+        .find({ email: req.params.email }).sort({price: -1}).toArray()
+      console.log(result);
+      res.send(result);
+    });
+
+    // update toy ---------------------------------------------------------------
+    app.put('/updatetoy/:id', async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatetoy = {
+        $set: {
+          price: body.price,
+          quantity: body.quantity,
+          description: body.description
+        },
+      }
+      const result = await toyCollection.updateOne(filter, updatetoy);
+      res.send(result)
+    });
+
+
+    // delete toy  ---------------------------------------------------------------
+    app.delete('/deletetoy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // category details ---------------------------------------------------------------
+
     app.get("/catedetails/:id", async (req, res) => {
       const id = req.params.id;
       const result = await toyCollection.findOne({ _id: new ObjectId(id) });
@@ -67,54 +135,6 @@ async function run() {
       }
     });
 
-        // all toy details ---------------------------------------------------------------
-    app.get("/alltoydetails/:id", async (req, res) => {
-      const id = req.params.id;
-      const result = await toyCollection.findOne({ _id: new ObjectId(id) });
-      if (result) {
-        res.send(result);
-      } else {
-        res.status(400).json({ status: 400, message: "No Data Found" });
-      }
-    });
-    
-    // add toy ---------------------------------------------------------------
-    app.post('/addToys', async (req, res) => {
-      const body = req.body;
-      const result = await toyCollection.insertOne(body);
-      res.send(result)
-    });
-
-        // my toys ---------------------------------------------------------------
-    app.get('/myToys/:id', async (req, res) => {
-      const result = await toyCollection.find({ email: req.params.id }).sort({price: -1}).toArray();
-      console.log(req.params.id);
-      res.send(result);
-    });
-
-        // update toy ---------------------------------------------------------------
-    app.put('/updatetoy/:id' , async(req, res) => {
-      const id = req.params.id;
-      const body = req.body;
-      const filter = {_id:new ObjectId(id)};
-      const updatetoy = {
-        $set:{
-          price: body.price,
-          quantity:body.quantity,
-          description : body.description
-        },
-      }
-      const result = await toyCollection.updateOne(filter, updatetoy);
-      res.send(result)
-    });
-
-        // delete toy  ---------------------------------------------------------------
-    app.delete('/deletetoy/:id' , async(req, res) => {
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await toyCollection.deleteOne(query);
-      res.send(result);
-    })
 
   } finally {
 
